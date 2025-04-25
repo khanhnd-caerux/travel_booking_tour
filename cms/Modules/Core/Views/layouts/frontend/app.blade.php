87c13666bd54c9682e5866baa37aee953d96e397
khanhnd771998 <khanhnd@caerux.com>
2025-04-25 15:10:14 +0700

    update

--- cms/Modules/Core/Views/layouts/frontend/app.blade.php
+++ cms/Modules/Core/Views/layouts/frontend/app.blade.php
@@ -37,7 +37,7 @@
     <meta property="og:image" content="images/config/logo_1697707579.png" />
     <meta property="og:image:width" content="600 " />
     <meta property="og:image:alt" content="Mama’s Homestay - Explore Ha Giang">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
     <link rel="canonical" href="index.html">
     <meta content="INDEX,FOLLOW" name="robots" />
@@ -53,18 +53,133 @@
     <link rel="alternate" type="application/rss+xml" title="Mama’s Homestay - Explore Ha Giang Feed" href="rss.html" />
     <meta name="google-site-verification" content="wbGxdRaMeMm69UxvryuTouLeXhZeiwc2FQYQI6C3kVM" />
 </head>
+
 <body>
     @include('Core::layouts.frontend.header')
     @yield('content')
     @include('Core::layouts.frontend.footer')
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-26NZ4VBNQ2"></script>
+
+    <div id='fixed-bar'>
+        <div id='bar-inner'>
+            <a class='go-top' href='#page-wrapper' title='Back to top'>
+                <svg x="0px" y="0px" viewBox="0 0 284.929 284.929" style="enable-background:new 0 0 284.929 284.929;"
+                    xml:space="preserve">
+                    <g>
+                        <path d="M282.082,195.285L149.028,62.24c-1.901-1.903-4.088-2.856-6.562-2.856s-4.665,0.953-6.567,2.856L2.856,195.285
+						C0.95,197.191,0,199.378,0,201.853c0,2.474,0.953,4.664,2.856,6.566l14.272,14.271c1.903,1.903,4.093,2.854,6.567,2.854
+						c2.474,0,4.664-0.951,6.567-2.854l112.204-112.202l112.208,112.209c1.902,1.903,4.093,2.848,6.563,2.848
+						c2.478,0,4.668-0.951,6.57-2.848l14.274-14.277c1.902-1.902,2.847-4.093,2.847-6.566
+						C284.929,199.378,283.984,197.188,282.082,195.285z" />
+                    </g>
+                </svg>
+            </a>
+        </div>
+    </div>
+    <input type="hidden" id="Itid" name="Itid" value="1">
+
+    <div class="megamenu_mb ">
+        <div class="close_all">
+            <div class="navicon-line"></div>
+            <div class="navicon-line"></div>
+        </div>
+        <ul class="menu scroll_bar">
+            <span class="label" id="menu_">Menu</span>
+            <li class='group_class_menu group_id_menu_27 level_0'><a title='About' href='introduce/index.html'>About</a>
+            </li>
+            <li class='group_class_menu group_id_menu_27 level_0'><a title='Tours' href='index.html#tour'>Tours</a></li>
+            <li class='group_class_menu group_id_menu_27 level_0'><a title='Homestay'
+                    href='homestay/index.html'>Homestay</a></li>
+            <li class='group_class_menu group_id_menu_27 level_0'><a title='News' href='news/index.html'>News</a></li>
+            <li class='group_class_menu group_id_menu_27 level_0'><a title='Contact'
+                    href='contact/index.html'>Contact</a></li>
+            <li class='level_1 '><a style='color: red' href='index.html#tour'>Book Now</a></li>
+        </ul>
+    </div>
+
+    <div class="right_support_online">
+        <div class='onlinesupport_list_row'>
+            <div class="gid-row">
+                <div class="item item_zalo item-show">
+                    <a rel="nofollow" title="Hỗ trợ instagram" target='_blank'
+                        href="https://www.instagram.com/hagiangloop/">
+                        <svg height="44px" width="44px" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" aria-label="Instagram" role="img"
+                            viewBox="0 0 512 512" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <rect width="512" height="512" rx="15%" id="b"></rect>
+                                <use fill="url(#a)" xlink:href="#b"></use>
+                                <use fill="url(#c)" xlink:href="#b"></use>
+                                <radialGradient id="a" cx=".4" cy="1" r="1">
+                                    <stop offset=".1" stop-color="#fd5"></stop>
+                                    <stop offset=".5" stop-color="#ff543e"></stop>
+                                    <stop offset="1" stop-color="#c837ab"></stop>
+                                </radialGradient>
+                                <linearGradient id="c" x2=".2" y2="1">
+                                    <stop offset=".1" stop-color="#3771c8"></stop>
+                                    <stop offset=".5" stop-color="#60f" stop-opacity="0"></stop>
+                                </linearGradient>
+                                <g fill="none" stroke="#ffffff" stroke-width="30">
+                                    <rect width="308" height="308" x="102" y="102" rx="81"></rect>
+                                    <circle cx="256" cy="256" r="72"></circle>
+                                    <circle cx="347" cy="165" r="6"></circle>
+                                </g>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+
+
+                <div class="item item-show">
+                    <a title="Hotline" target="_blank" href="https://wa.me/+84849278787">
+                        <svg height="44px" width="44px" xmlns="http://www.w3.org/2000/svg" aria-label="WhatsApp"
+                            role="img" viewBox="0 0 512 512" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <rect width="512" height="512" rx="15%" fill="#25d366"></rect>
+                                <path fill="#25d366" stroke="#ffffff" stroke-width="26"
+                                    d="M123 393l14-65a138 138 0 1150 47z"></path>
+                                <path fill="#ffffff"
+                                    d="M308 273c-3-2-6-3-9 1l-12 16c-3 2-5 3-9 1-15-8-36-17-54-47-1-4 1-6 3-8l9-14c2-2 1-4 0-6l-12-29c-3-8-6-7-9-7h-8c-2 0-6 1-10 5-22 22-13 53 3 73 3 4 23 40 66 59 32 14 39 12 48 10 11-1 22-10 27-19 1-3 6-16 2-18">
+                                </path>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="item item-show">
+                    <a title="Gmail" target="" href="mailto:Mamashomestayhg@gmail.com">
+                        <svg height="44px" width="44px" xmlns="http://www.w3.org/2000/svg" aria-label="Gmail" role="img"
+                            viewBox="0 0 512.00 512.00" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <rect width="512" height="512" rx="15%" fill="#ffffff"></rect>
+                                <path d="M158 391v-142l-82-63V361q0 30 30 30" fill="#4285f4"></path>
+                                <path d="M 154 248l102 77l102-77v-98l-102 77l-102-77" fill="#ea4335"></path>
+                                <path d="M354 391v-142l82-63V361q0 30-30 30" fill="#34a853"></path>
+                                <path d="M76 188l82 63v-98l-30-23c-27-21-52 0-52 26" fill="#c5221f"></path>
+                                <path d="M436 188l-82 63v-98l30-23c27-21 52 0 52 26" fill="#fbbc04"></path>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script async='async' language="javascript" type="text/javascript"
+        src="{{asset('frontend/cache/js/1339e18ad29b457616a4a343f51780b91520.js?20250419171143')}}">
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-26NZ4VBNQ2"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
+        function gtag() {dataLayer.push(arguments); }
         gtag('js', new Date());
 
         gtag('config', 'G-26NZ4VBNQ2');
     </script>
 </body>
+
 </html>
